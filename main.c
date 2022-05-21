8583a39fe626332af094633557c29179de572934
pedro <pedro_freires18@outlook.com>
2022-05-21 17:25:07 -0300

    inserindo esboço de criar servidor, testando o save in file

--- main.c
+++ main.c
@@ -4,13 +4,18 @@
 
 #include "estruturas.h"
 #include "entradas.h"
-#include "arquivos.h"
-#include "utilitarios.h"
+//#include "arquivos.h"
+//#include "utilitarios.h"
+#include "servidor.h"
 
 int main(int argc, char *argv[])
 {
-    servidor * _servidor = malloc(MAX*sizeof(servidor));
+    char aux,nome[255],siape[255],cpf[255],nasci[255],rg[255],tipo[255],salario[255],endereco[255];
+    //servidor * _servidor = malloc(MAX*sizeof(servidor));
+    servidor * _servidor[MAX];
+    limpar_vet_ptrs(_servidor);
 
+    fflush(stdin);
     char input = '~';
 
     do
@@ -26,7 +31,7 @@ int main(int argc, char *argv[])
         printf("0. Terminar programa\n\n");
 
         printf("Escolha: ");
-        ler_menu(input);
+        input = ler_menu(input);
         
         switch(input)
         {
@@ -35,7 +40,36 @@ int main(int argc, char *argv[])
                 //escrever_arquivo();
                 return 0;
             case inserir_servidor:
-                //
+        
+                //do {
+                printf("Digite o nome do Servidor : \n:");
+                fgets(nome,sizeof(nome),stdin);
+                fflush(stdin);
+                printf("Digite o siape do Servidor : \n:");
+                fgets(siape,sizeof(siape),stdin);
+                fflush(stdin);
+                printf("Digite o cpf do Servidor : \n:");
+                fgets(cpf,sizeof(cpf),stdin);
+                fflush(stdin);
+                //strcpy(aux,'1');
+
+                //}while( checa_branco(nome,siape,cpf,aux) );
+
+                printf("Digite a data de nascimento do Servidor : \n:");
+                fgets(nasci,sizeof(nasci),stdin);
+                fflush(stdin);
+                printf("Digite o rg do Servidor : \n:");
+                fgets(rg,sizeof(rg),stdin);
+                fflush(stdin);
+                printf("Digite o salario do servidor : \n:");
+                fgets(salario,sizeof(salario),stdin);
+                fflush(stdin);
+                printf("Digite o endereco do Servidor : \n:");
+                fgets(endereco,sizeof(endereco),stdin);
+                fflush(stdin);
+
+                criar_servidor(nome,siape,cpf,nasci,rg,tipo,salario,endereco,_servidor);
+                
                 break;
             case alterar_servidor:
                 //
@@ -44,7 +78,29 @@ int main(int argc, char *argv[])
                 //
                 break;
             case listar_servidor:
-                //
+                printf("Digite a forma como deseja printar os servidores\n\n");
+                printf("1. Printar apenas os Tecnicos \n");
+                printf("2. Printar apenas os professores \n");
+                printf("3. Printar Todos \n");
+                printf("4. Printar um servidor pelo cod dele.\n");
+                printf("5. Para retornar ao menu\n");
+                
+                scanf("%c",&input);
+                fflush(stdin);
+
+                if( input == '1' || input == '2' || input == '3' )
+                {
+                    list_serv(_servidor);
+                    //copying_nomes(input);
+                }else if( input == '4'){
+                    //print_serv_cod();
+                }else if(input == '5'){
+                    printf("\n\n");
+                    break;
+                }else{
+                    printf("\nEssa opção não existe !\n");
+                    break;
+                }
                 break;
             case inserir_veiculo:
                 //
@@ -64,7 +120,7 @@ int main(int argc, char *argv[])
 
     } while (input != '0');
 
-    free(_servidor);
-
+    //free(_servidor);
+    libera_memoria(_servidor);
     return 0;
 }
